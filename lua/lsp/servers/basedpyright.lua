3d85559606ba75088e5bbe6d67257097dd559f53
xzx97 <xzx971105@outlook.com>
2025-02-27 17:29:29 +0800

    remove plugin: nvim-dev-container, and change the files permission to 664

--- lua/lsp/servers/basedpyright.lua
+++ lua/lsp/servers/basedpyright.lua
@@ -3,7 +3,7 @@ local lspconfig = require('lspconfig')
 lspconfig.basedpyright.setup {
 	settings = {
 		python = {
-			pythonPath = vim.env.CONDA_PREFIX and vim.env.CONDA_PREFIX .. "/bin/python" or "/usr/bin/python",
+			pythonPath = "/bin/python" or "/usr/bin/python",
 		},
 	},
 }
--- lua/plugins/dev-container.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"nvim-dev-container/nvim-dev-container",
-		event = "VeryLazy",
-		config = function ()
-			require("devcontainer").setup{}
-		end
-	}
-}
