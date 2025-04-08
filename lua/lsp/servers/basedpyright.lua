9f2171334210d1d842c58f7cb07c08d52cadcc2c
xzx97 <xzx971105@outlook.com>
2025-04-08 16:38:24 +0800

    save for transforming to Mac

--- lua/lsp/servers/basedpyright.lua
+++ lua/lsp/servers/basedpyright.lua
@@ -1,3 +1,16 @@
 local lspconfig = require('lspconfig')
 
-lspconfig.basedpyright.setup({})
+lspconfig.basedpyright.setup({
+	settings = {
+		basedpyright = {
+			-- Using Ruff's import organizer
+			disableOrganizeImports = true,
+		},
+		python = {
+			analysis = {
+				-- Ignore all files for analysis to exclusively use Ruff for linting
+				ignore = { '*' },
+			},
+		},
+	},
+})
--- lua/plugins/plugin-null-ls-nvim.lua
+++ lua/plugins/plugin-null-ls-nvim.lua
@@ -7,7 +7,8 @@ return {
 
 			null_ls.setup({
 				sources = {
-					null_ls.builtins.formatting.ruff
+					null_ls.builtins.formatting.ruff,
+					null_ls.builtins.diagnostics.ruff
 				},
 			})
 		end,
