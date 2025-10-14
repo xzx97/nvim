2babcfab0e4ce4e3b44b86d602b550d6ea1ae5e9
xzx97 <xzx971105@outlook.com>
2025-10-14 11:10:52 +0800

    comment none-ls builtin ruff config

---
+++ lua/plugins/plugin-none-ls-nvim.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		event = "VeryLazy",
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				sources = {
+					-- null_ls.builtins.formatting.ruff,
+					-- null_ls.builtins.diagnostics.ruff
+				},
+			})
+		end,
+	},
+}
