858f929f25c55fc8f5326f35e828f8c2ddc2b488
xzx97 <xzx971105@outlook.com>
2025-12-22 14:39:39 +0800

    change aerial.nvim default icons and remap the keybindings

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -1,16 +1,66 @@
 return {
-  {
-    "stevearc/aerial.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("aerial").setup()
+  "stevearc/aerial.nvim",
+  event = "VeryLazy",
+  opts = function()
+    -- 通用 Nerd Font / Unicode 图标，宽度安全
+    local icons = {
+      File          = "",
+      Module        = "",
+      Namespace     = "",
+      Package       = "",
+      Class         = "",
+      Method        = "",
+      Property      = "",
+      Field         = "",
+      Constructor   = "",
+      Enum          = "",
+      Interface     = "",
+      Function      = "󰊕",   -- 更安全的 Function 符号
+      Variable      = "",
+      Constant      = "",
+      String        = "",
+      Number        = "",
+      Boolean       = "◩",
+      Array         = "",
+      Object        = "",
+      Key           = "",
+      Null          = "∅",   -- Unicode 安全
+      EnumMember    = "",
+      Struct        = "",
+      Event         = "",
+      Operator      = "",
+      TypeParameter = "",
+    }
 
-      -- keybindings
-      vim.keymap.set("n", "<leader>t", "<cmd>AerialToggle<CR>", { desc = "Toggle Aerial outline" })
-      vim.keymap.set("n", "<leader>T", "<cmd>AerialNavOpen<CR>", { desc = "Open Aerial navigation" })
-    end,
-  }
+    local opts = {
+      attach_mode = "global",
+      backends = { "lsp", "treesitter", "markdown", "man" },
+      show_guides = true,
+      layout = {
+        resize_to_content = false,
+        win_opts = {
+          winhl = "Normal:NormalFloat,FloatBorder:NormalFloat,SignColumn:SignColumnSB",
+          signcolumn = "yes",
+          statuscolumn = " ",
+        },
+      },
+      icons = icons,
+      filter_kind = false,
+      guides = {
+        mid_item   = "├╴",
+        last_item  = "└╴",
+        nested_top = "│ ",
+        whitespace = "  ",
+      },
+    }
+    return opts
+  end,
+
+	keys = {
+  -- 侧边栏
+  { "<leader>T", "<cmd>AerialToggle<CR>", desc = "Toggle Aerial floating window" },
+  -- 浮动窗口
+  { "<leader>t", "<cmd>AerialNavOpen<CR>", desc = "Open Aerial side panel" },
+	}
 }
+
